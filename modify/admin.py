8f779733f3d093b49c05565b61dfb2ae1bda0d5c
Jack Semancik <jack.semancik@yahoo.com>
2017-08-24 18:07:37 +0900

    add About and Skills classes to admin panel

--- modify/admin.py
+++ modify/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from storefrontend.models import Products
+from storefrontend.models import Products, Skills, About
 
 # Register your models here.
 class ProductsAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
+class SkillsAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title','description','icon',)
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('id',)
+    search_fields = ('about',)
 
 admin.site.register(Products, ProductsAdmin)
+admin.site.register(Skills, SkillsAdmin)
+admin.site.register(About, AboutAdmin)
