6feb8cad5f1f703d11d6b1bb651203dfb03ef030
Jack Semancik <jack.semancik@yahoo.com>
2017-08-24 17:37:59 +0900

    Add about_text and skills views

--- storefrontend/views.py
+++ storefrontend/views.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.shortcuts import render_to_response
-from storefrontend.models import Products
+from storefrontend.models import Products, About, Skills
 
 # Create your views here.
 def homeserve(request):
     products = Products.objects.all()
-    return render_to_response('home.html', {'products': products})
+    about_text = About.objects.get(id=0)
+    skills = Skills.objects.all()
+    return render_to_response('home.html', {'products': products, 'about_text':about_text, 'skills':skills})
 
 def productserve(request, value):
     productpage = Products.objects.filter(slug__exact=value)
