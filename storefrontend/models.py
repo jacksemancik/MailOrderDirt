e4503aab9f82f948ccd1303614802f0e7c269a96
Jack Semancik <jack.semancik@yahoo.com>
2017-08-25 19:11:28 +0900

    add contact model for corresponding contact form

--- storefrontend/models.py
+++ storefrontend/models.py
@@ -22,3 +22,12 @@ class Skills(models.Model):
     description = models.TextField()
     def __str__(self):
         return self.title
+
+class ContactModel(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    def __str__(self):
+        return self.email
